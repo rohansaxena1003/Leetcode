9d3b7825c9e925e4619725afabea661ba882f968
Rohan Saxena <rohansaxena1003@gmail.com>
2023-07-27 00:49:07 +0530

    26 July code complete

--- Questions/1852-MinSpeedToArriveOnTime.js
+++ Questions/1852-MinSpeedToArriveOnTime.js
@@ -4,15 +4,20 @@
  * @return {number}
  */
 var minSpeedOnTime = function(dist, hour) {
+    
+    if( Math.ceil(hour) < dist.length) {
+        return -1;
+    }
+
     let min = 1;
-    let max = dist.reduce( (a,b) => Math.max(a,b));
+    let max = dist.reduce( (a,b) => Math.max(a,b)) * 100;
     
 
     while(min <= max) {
         let mid = Math.floor( ((max-min)/2) + min );
-        console.log(mid);
+        console.log("mid = " + mid);
         let hcal = totalTime(dist, mid);
-
+        console.log("hcal = " + hcal);
         if(hcal == hour) {
             return mid;
         } else if(hcal > hour) {
@@ -21,19 +26,22 @@ var minSpeedOnTime = function(dist, hour) {
             max = mid - 1;
         }
     }
+
+    console.log("min=" + min + ",  max =" + max);
+    return min;
 };
 
 function totalTime(dist, time) {
     let hcal = 0;
 
-    for(let d of dist ) {
-        hcal = hcal + Math.ceil(d/time);
+    for(let i = 0; i < dist.length - 1; i++ ) {
+        hcal = hcal + Math.ceil(dist[i]/time);
     }
 
-    return hcal;
+    return hcal + (dist[dist.length - 1]/time);
 }
 
-// console.log(minSpeedOnTime([1,5,8,7,4], 6));
+console.log(minSpeedOnTime([1,5,8,7,4], 6));
 // console.log(minSpeedOnTime([1,3,2], 6));
-// console.log(minSpeedOnTime([1,5,8,7,4], 6));
-console.log(minSpeedOnTime([1,3,2], 2.7));
+console.log(minSpeedOnTime([1,3,2], 1.99));
+// console.log(minSpeedOnTime([1,3,2], 2.7));
